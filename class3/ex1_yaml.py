ae22bae38b12254d284509cdf7ca621339a25e8a
Harry Sun <zoomhgtk@gmail.com>
2016-08-22 06:28:45 -0700

    next step: determine whether the OID has been changed

---
+++ class3/ex1_yaml.py
@@ -0,0 +1,53 @@
+import snmp_helper
+from datetime import datetime
+import yaml
+# Uptime when running config last changed
+
+ip_addr1 = "184.105.247.70"
+ip_addr2 = "184.105.247.71"
+
+port = 161
+
+snmp_user = 'pysnmp'
+auth_key = 'galileo1'
+encrypt_key = 'galileo1'
+
+a_user = (snmp_user, auth_key, encrypt_key)
+
+device1 = (ip_addr1, port)
+device2 = (ip_addr2, port)
+
+def get_the_current_value(device, user):
+     
+    ccmHistoryRunningLastChanged = '1.3.6.1.4.1.9.9.43.1.1.1.0'
+    output = snmp_helper.snmp_get_oid_v3(device, user, ccmHistoryRunningLastChanged)
+    initial = snmp_helper.snmp_extract(output)
+    return initial
+
+value1 = get_the_current_value(device1, a_user)
+
+def save_status(filename, value):
+    '''
+    save the current value of ccmHistoryRunningLastChanged
+    '''
+
+    status = []
+    status.append({})
+    status[-1]['time_stemp'] = str(datetime.now())
+    status[-1]['value'] = value
+    with open(filename, "a") as stream:
+        stream.write(yaml.dump(status, default_flow_style = False))
+    print "the following data saved"
+    print status
+
+def read_status(filename):
+    '''
+    read the value of ccmHistoryRunningLastChanged from yaml file
+    '''
+    with open(filename, "r") as stream:
+        current = yaml.load(stream)
+        print current[-1]
+
+save_status("ex1_status.yml", value1)
+
+read_status("ex1_status.yml")
