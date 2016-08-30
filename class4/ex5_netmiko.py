3b5ba68caced89522aff9ab5adc0302c9740a6ae
Harry Sun <zoomhgtk@gmail.com>
2016-08-30 06:58:23 -0700

    ex5 done

---
+++ class4/ex5_netmiko.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+rtr2 = {
+    'device_type': 'cisco_ios',
+    'ip': '184.105.247.71',
+    'username': 'pyclass',
+    'secret': '',
+    'port': '22',
+    'password': '88newclass'
+    }
+
+conn1 = ConnectHandler(**rtr2)
+conn1.config_mode()
+
+if conn1.check_config_mode():
+    print "here you are in config mode"
+    print "your prompt is :",
+    print conn1.find_prompt()
+else:
+    print "you are not in config mode"
+    print "your prompt is :",
+    print conn1.find_prompt()
