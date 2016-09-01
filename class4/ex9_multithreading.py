5d21f68c3c365aa376073f34c2bc4a54ecd49e3b
Harry Sun <zoomhgtk@gmail.com>
2016-09-01 09:04:44 -0700

    the Lock can not solve the messy print, sys.stdout.write(str1) can solve

--- class4/ex9_multithreading.py
+++ class4/ex9_multithreading.py
@@ -4,6 +4,7 @@ import threading
 from netmiko import ConnectHandler
 from getpass import getpass
 import sys
+import time
 
 rtr1 = {
     'device_type': 'cisco_ios',
@@ -45,24 +46,55 @@ def printARP(device):
     print the ARP table on this device
     '''
     conn = ConnectHandler(**device)
-    sys.stdout.flush()
-    print "ARP table on %s :" % get_name_of_obj(device)
+    #sys.stdout.flush()
+
     arp = conn.send_command("show arp")
-    print arp
-    print "*********************\n"
+    time.sleep(1)
+    outp =  ("ARP table on %s :" % get_name_of_obj(device, "each_device")+"\n")
+    # use find_prompt() to determin the output of threads are not messed up
+    outp +=  (conn.find_prompt()+"\n")
+    outp +=  (arp+"\n")
+    outp += "*********************\n"
+    sys.stdout.write(outp)
 
-#class myThread(threading.Thread):
-
+class myThread(threading.Thread):
+    def __init__(self,  device):
+        threading.Thread.__init__(self)
+        self.device = device
+    def run(self):
+        threadLock1 = threading.Lock()
+        threadLock1.acquire()
+        printARP(self.device)
+        threadLock1.release()
 
 def main():
     '''
     using threading here
     '''
-    threadObj1 = threading.Thread(target=printARP, args=[rtr1])
-    threadObj2 = threading.Thread(target=printARP, args=[rtr2])
+    thread1 = threading.Thread(target=printARP, args=[rtr1])
+    thread2 = threading.Thread(target=printARP, args=[rtr2])
+    thread3 = threading.Thread(target=printARP, args=[srx])
 
-    threadObj1.start()
-    threadObj2.start()
 
+    '''
+    thread1 = myThread(rtr1)
+    thread2 = myThread(rtr2)
+    thread3 = myThread(srx)
+    '''
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+      
+    threads = []
+    threads.append(thread1)
+    threads.append(thread2)
+    threads.append(thread3)
+
+
+    for t in threads:
+        t.join()
+
+    print "All done"
 if __name__ == "__main__":
     main()
